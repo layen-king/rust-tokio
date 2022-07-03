f42eda65bbd2e861211e2ef45c13df3a0936b170
layen-king <446495695@qq.com>
2022-07-03 22:11:57 +0800

    feat: 使用RwLoack + once_cell

--- examples/pow1.rs
+++ examples/pow1.rs
@@ -1,8 +1,13 @@
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{mpsc, Arc};
+use std::sync::{mpsc, RwLock};
 use std::thread;
+use once_cell::sync::Lazy;
+
+/// 全局锁
+static IS_SOLUTION_FOUND:Lazy<RwLock<bool>> = Lazy::new(||{
+  RwLock::new(false)
+});
 
 /// 基准值
 const BASE: usize = 42;
@@ -19,15 +24,12 @@ fn main() {
     );
     println!("Started {} threads", THREADS);
     println!("please wait...");
-    // 全局原子
-    let is_solution_found = Arc::new(AtomicBool::new(false));
     // 通道
     let (sender, receiver) = mpsc::channel();
     for i in 0..THREADS {
         // 复制发送者
         let sender_n = sender.clone();
-        let is_solution_found = is_solution_found.clone();
-        thread::spawn(move || find(i, sender_n, is_solution_found));
+        thread::spawn(move || find(i, sender_n));
     }
     match receiver.recv() {
         Ok(Solution(i, hash)) => {
@@ -53,17 +55,17 @@ fn verify(number: usize) -> Option<Solution> {
 }
 
 /// 查找数字
-fn find(start_at: usize, sender: mpsc::Sender<Solution>, is_solution_found: Arc<AtomicBool>) {
+fn find(start_at: usize, sender: mpsc::Sender<Solution>) {
     // 根据进程无限步进
     for number in (start_at..).step_by(THREADS) {
         // 若全局的原子为true,表明其他线程已经找到,直接返回
-        if is_solution_found.load(Ordering::Relaxed) {
+        if *IS_SOLUTION_FOUND.read().unwrap() {
             return;
         }
         // 若数字符合条件
         if let Some(solution) = verify(number) {
             // 修改全局
-            is_solution_found.store(true, Ordering::Relaxed);
+            *IS_SOLUTION_FOUND.write().unwrap() = true;
             // 通过通道发送
             sender.send(solution).unwrap();
             return;
