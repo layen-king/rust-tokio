cc3c44e2c88d624f53b9a1b69cf5166a5b4c62b0
layen-king <446495695@qq.com>
2022-07-03 11:27:10 +0800

    styles: 注释

--- examples/pow.rs
+++ examples/pow.rs
@@ -2,7 +2,7 @@ use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{mpsc, Arc};
-use std::{thread};
+use std::thread;
 
 /// 基准值
 const BASE: usize = 42;
@@ -19,9 +19,12 @@ fn main() {
     );
     println!("Started {} threads", THREADS);
     println!("please wait...");
+    // 全局原子
     let is_solution_found = Arc::new(AtomicBool::new(false));
+    // 通道
     let (sender, receiver) = mpsc::channel();
     for i in 0..THREADS {
+        // 复制发送者
         let sender_n = sender.clone();
         let is_solution_found = is_solution_found.clone();
         thread::spawn(move || find(i, sender_n, is_solution_found));
@@ -51,15 +54,15 @@ fn verify(number: usize) -> Option<Solution> {
 
 /// 查找数字
 fn find(start_at: usize, sender: mpsc::Sender<Solution>, is_solution_found: Arc<AtomicBool>) {
-  // 根据进程无限步进
+    // 根据进程无限步进
     for number in (start_at..).step_by(THREADS) {
-      // 若全局的原子为true,表明其他线程已经找到,直接返回
+        // 若全局的原子为true,表明其他线程已经找到,直接返回
         if is_solution_found.load(Ordering::Relaxed) {
             return;
         }
         // 若数字符合条件
         if let Some(solution) = verify(number) {
-          // 修改全局
+            // 修改全局
             is_solution_found.store(true, Ordering::Relaxed);
             // 通过通道发送
             sender.send(solution).unwrap();
